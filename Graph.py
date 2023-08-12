9d6e035006a592c2ad6c35c43822c5119300af92
Ayushman Kumar <srivastava.zing@gmail.com>
2023-08-12 12:48:37 +0530

    commit

--- Graph.py
+++ Graph.py
@@ -1,6 +1,6 @@
 import os
 from random import randint
-for i in range(1, 365):
+for i in range(1, 1000):
     for J in range(0, randint(1, 10)):
         d = str(i) + 'days ago'
         with open('file.text', 'a') as file:
